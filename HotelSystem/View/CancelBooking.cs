66156b8110d8479be63545f7f802c7834d9e7dbf
Blessings Nyameni <nymndy001@myuct.ac.za>
2025-09-24 21:04:13 +0200

    did some stuff

--- HotelSystem/View/CancelBooking.cs
+++ HotelSystem/View/CancelBooking.cs
@@ -16,5 +16,10 @@ namespace HotelSystem.View
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
