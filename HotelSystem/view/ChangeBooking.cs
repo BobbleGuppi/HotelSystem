2f8b85da704c04e894dc3a8da8b8113b853ed9db
Rachel Alia <ALXRAC002@myuct.ac.za>
2025-09-24 20:17:59 +0200

    renamed my form

--- HotelSystem/view/ChangeBooking.cs
+++ HotelSystem/view/ChangeBooking.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace HotelSystem.View
 {
-    public partial class Form4 : Form
+    public partial class ChangeBooking : Form
     {
-        public Form4()
+        public ChangeBooking()
         {
             InitializeComponent();
         }
