e3707e2c20223ebdd66ed91ea83b166b15241b9a
Rachel Alia <ALXRAC002@myuct.ac.za>
2025-09-24 20:41:56 +0200

    Added form

--- HotelSystem/view/ChangeBooking.Designer.cs
+++ HotelSystem/view/ChangeBooking.Designer.cs
@@ -46,11 +46,13 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(520, 210);
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Eras Demi ITC", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(121, 109);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(417, 31);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Provide the reservation number";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // textBox1
