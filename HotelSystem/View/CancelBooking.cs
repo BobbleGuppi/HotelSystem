d2ecce6a84d23fb40e8f393662cfd0ae4be1abd3
Blessings Nyameni <nymndy001@myuct.ac.za>
2025-09-24 20:19:12 +0200

    yeah

--- HotelSystem/View/CancelBooking.cs
+++ HotelSystem/View/CancelBooking.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace HotelSystem.View
 {
-    public partial class Form2 : Form
+    public partial class CancelBooking : Form
     {
-        public Form2()
+        public CancelBooking()
         {
             InitializeComponent();
         }
