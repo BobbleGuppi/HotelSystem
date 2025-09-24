051409404366860e12eb9e1ae507a6bb70077007
Blessings Nyameni <nymndy001@myuct.ac.za>
2025-09-24 20:46:50 +0200

    my form

--- HotelSystem/View/CancelBooking.Designer.cs
+++ HotelSystem/View/CancelBooking.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HotelSystem.View
 {
-    partial class Form1
+    partial class CancelBooking
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // CancelBooking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(893, 570);
+            this.Name = "CancelBooking";
+            this.Text = "CancelBooking";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- HotelSystem/View/CancelBooking.cs
+++ HotelSystem/View/CancelBooking.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace HotelSystem.View
 {
-    public partial class Form1 : Form
+    public partial class CancelBooking : Form
     {
-        public Form1()
+        public CancelBooking()
         {
             InitializeComponent();
         }
