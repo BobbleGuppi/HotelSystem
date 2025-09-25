184dcded2c0657c5a6ea9770a2c450d37970e59f
Zamashengu <zamashengut@gmail.com>
2025-09-25 19:30:19 +0200

    added attributes to classes

--- HotelSystem/Logic/Payment.cs
+++ HotelSystem/Logic/Payment.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace HotelSystem.Logic
 {
-    internal class Payment
+    public class Payment
     {
     }
 }
--- HotelSystem/Logic/Reservation.cs
+++ HotelSystem/Logic/Reservation.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace HotelSystem.Logic
 {
-    internal class Reservation
-    {
+     public class Reservation
+     {
+        private string reservationID;
+        private int roomID; //thinking we should make it an enum
+        private Guest guest;
+        private DateTime checkInDate; //we will have to set a standard time
+        private DateTime checkOutDate;
+        private double totalPrice;
+        private bool depositPaid;
+
+     public Reservation(string reservationID, int roomID, Guest guest, DateTime checkInDate, DateTime checkOutDate, double totalPrice, bool depositPaid)
+        {
+            this.reservationID = reservationID;
+            this.roomID = roomID;
+            this.guest = guest;
+            this.checkInDate = checkInDate;
+            this.checkOutDate = checkOutDate;
+            this.totalPrice = totalPrice;
+            this.depositPaid = depositPaid;
+        }
+
     }
 }
