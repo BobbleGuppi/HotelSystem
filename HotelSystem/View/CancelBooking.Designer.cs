94fdcf092242fe7afa0885604e808795d87203a0
Blessings Nyameni <nymndy001@myuct.ac.za>
2025-09-27 16:24:46 +0200

    color

--- HotelSystem/View/CancelBooking.Designer.cs
+++ HotelSystem/View/CancelBooking.Designer.cs
@@ -34,6 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.ClientSize = new System.Drawing.Size(942, 534);
             this.Name = "CancelBooking";
             this.Text = "CancelBooking";
