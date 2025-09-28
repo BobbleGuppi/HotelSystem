3eeac6ad96917609371f2d0fc3d12d64a9862585
Zamashengu <zamashengut@gmail.com>
2025-09-28 15:25:52 +0200

    created DB class and Reservation Database

---
+++ HotelSystem/Database/DB.cs
@@ -0,0 +1,68 @@
+﻿using HotelSystem.Properties;
+using HotelSystem.Properties;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HotelSystem.Database
+{
+    public class DB
+    {
+        //private string strConn = Settings.Default.EmployeeDatabaseConnectionString;
+        protected SqlConnection cnMain;
+        protected DataSet dsMain;
+        protected SqlDataAdapter daMain;
+        public enum DBOperation
+        {
+            Add = 0,
+            Edit = 1,
+            Delete = 2
+        }
+        public void FillDataSet(string aSQLstring, string aTable)
+        {
+            //fills dataset fresh from the db for a specific table and with a specific Query
+            try
+            {
+                daMain = new SqlDataAdapter(aSQLstring, cnMain);
+                cnMain.Open();
+                //dsMain.Clear();
+                daMain.Fill(dsMain, aTable);
+                cnMain.Close();
+            }
+            catch (Exception errObj)
+            {
+                MessageBox.Show(errObj.Message + "  " + errObj.StackTrace);
+            }
+        }
+        protected bool UpdateDataSource(string sqlLocal, string table)
+        {
+            bool success;
+            try
+            {
+                //open the connection
+                cnMain.Open();
+                //***update the database table via the data adapter
+                daMain.Update(dsMain, table);
+                //---close the connection
+                cnMain.Close();
+                //refresh the dataset
+                FillDataSet(sqlLocal, table);
+                success = true;
+            }
+            catch (Exception errObj)
+            {
+                MessageBox.Show(errObj.Message + "  " + errObj.StackTrace);
+                success = false;
+            }
+            finally
+            {
+            }
+            return success;
+        }
+    }
+}
