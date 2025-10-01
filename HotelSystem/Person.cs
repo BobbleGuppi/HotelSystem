f1c646ab4847c0842254924b4e1a05dd58dc00cc
Rachel Alia <ALXRAC002@myuct.ac.za>
2025-10-01 18:44:14 +0200

    Created the GuestDB and edited the Guest and Person class to accomodate it

--- HotelSystem/Person.cs
+++ HotelSystem/Person.cs
@@ -8,11 +8,23 @@ namespace HotelSystem
 {
     public class Person
     {
+        #region Fields
         protected string id;
         protected string name;
         protected string phone;
         protected string address;
+        #endregion
 
+
+        #region Property Methods
+        public string ID { get { return id; } set { id = value; } }
+        public string Name { get { return name; } set { name = value; } }
+        public string Phone { get { return phone; } set { phone = value; } }
+        public string Address { get { return address; } set { address = value; } }
+        #endregion
+
+
+        #region Constructor
         public Person(string ID, string name, string phone, string address)
         {
             this.id = ID;
@@ -20,10 +32,16 @@ namespace HotelSystem
             this.phone = phone;
             this.address = address;
         }
+        #endregion
 
+
+        #region Methods
         public virtual string displayInfo()
         {
             return "$Full name:{name}\tAddress:{addresss}\tID:{id}";
         }
+        #endregion
+
+
     }
 }
