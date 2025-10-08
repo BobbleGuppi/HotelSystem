564403b7dec5ce7e43780669ba54ee774d2b0c5d
Zamashengu <zamashengut@gmail.com>
2025-10-08 14:05:28 +0200

    updated reservation logic

--- HotelSystem/Logic/Reservation.cs
+++ HotelSystem/Logic/Reservation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 namespace HotelSystem.Logic
 {
@@ -15,6 +16,7 @@ namespace HotelSystem.Logic
         private DateTime checkOutDate;
         public double totalPrice;
         private bool depositPaid = false;
+        private Collection<Room> rooms;
 
         public Reservation(string reservationID, int roomID, string guestID, DateTime checkInDate, DateTime checkOutDate, double totalPrice, bool depositPaid)
         {
@@ -105,6 +107,135 @@ namespace HotelSystem.Logic
             depositPaid = true;
         }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         public void changeReservationDates(DateTime newCheckIn, DateTime newCheckOut)
         {
             checkInDate = newCheckIn;
@@ -120,10 +251,22 @@ namespace HotelSystem.Logic
 
         }
 
+        public bool RoomAvailable(Collection<Room> rooms, DateTime checkIn, DateTime checkOut)
+        {
+            foreach (Room room in rooms)
+            {
+                if (room.IsAvailable(checkIn, checkOut))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
 
-        
-        
-     
+
+
+
+
 
     }
 }
